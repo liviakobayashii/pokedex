1b2ef7d8c183866237517941a1eead1bbf732fb8
liviakobayashii <liviakmm10@gmail.com>
2024-07-09 20:55:07 -0300

    feat: criação do projeto pokedex

---
+++ src/js/script.js
@@ -0,0 +1,68 @@
+let sectionButtons = document.querySelector("#buttons")
+let sectionPokemons = document.querySelector("#pokemons")
+let sectionInfoPokemons= document.querySelector("#infoPokemons")
+
+// const clicked = async(pokemon) => {
+//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+//     const json = await response.json()
+
+//     console.log(json.types["0"]["type"]["name"])
+// }
+const init = async() => {
+
+    const response = await fetch("https://pokeapi.co/api/v2/type")
+    const json = await response.json()
+    
+    const types = json.results
+    
+    let html = ""
+    types.forEach(type => {
+        html += `
+        <button onclick="showPokemons('${type.name}')">${type.name}</button>
+        `       
+    }); 
+
+    sectionButtons.innerHTML = html
+    sectionPokemons.innerHTML =""
+    sectionInfoPokemons.innerHTML = ""
+
+}
+init()
+
+const showPokemons = async(type) => {
+    
+    const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`)
+    const json = await response.json()
+    
+    const pokemons = json.pokemon
+    
+    let html = ""
+    pokemons.forEach(pokemon => {
+        html += `<button onclick="showInfoPokemons('${pokemon.pokemon.name}')">${pokemon.pokemon.name}</button>`
+        
+        sectionButtons.innerHTML = ""
+        sectionPokemons.innerHTML = html
+        sectionInfoPokemons.innerHTML = ""
+
+    });
+    
+}
+
+const showInfoPokemons = async(pokemon) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    const json = await response.json()
+    console.log(json)
+
+    const srcImg = json["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"]
+
+    let html = `<img src="${srcImg}" alt="pokemon">`
+
+    sectionPokemons.innerHTML =""
+    sectionButtons.innerHTML = ""
+    sectionInfoPokemons.innerHTML = html
+
+    
+}
+// buttonTeste.addEventListener("click", (e) => {
+//     clicked()
+// })
