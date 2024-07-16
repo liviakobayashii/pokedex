fcc3ffe06175da45b82d4f2e0a3ca12f0294c90b
liviakobayashii <liviakmm10@gmail.com>
2024-07-16 09:20:48 -0300

    style: mudança na estilização do projeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,60 +1,63 @@
-let sectionButtons = document.querySelector("#buttons")
 let sectionPokemons = document.querySelector("#pokemons")
-let sectionInfoPokemons= document.querySelector("#infoPokemons")
+let sectionInfoPokemons = document.querySelector("#infoPokemons")
 
-var pokemonType = ""
+var lastPokemonTypePicked = null
 
-const init = async() => {
+const init = async () => {
 
     const response = await fetch("https://pokeapi.co/api/v2/type")
     const json = await response.json()
-    
+
     const types = json.results
-    
+
     let pokemonTypes = ""
+    
     types.forEach(type => {
         pokemonTypes += `
         <button onclick="showPokemons('${type.name}')">${type.name}</button>
-        `       
-    }); 
+        `
+    });
 
     sectionPokemons.innerHTML = pokemonTypes
     sectionInfoPokemons.innerHTML = ""
 
 }
-init()
 
-const showPokemons = async(type) => {
-    
+const showPokemons = async (type) => {
+
+    lastPokemonTypePicked = type
+
+    const buttonToBack = `
+    <div id="backToHome">
+        <button onclick="init()" class="toBack"><-</button>
+    </div>
+    `
+
     const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`)
     const json = await response.json()
 
-    pokemonType = type
-    
     const pokemons = json.pokemon
-    
+
     let html = ""
     pokemons.forEach(pokemon => {
         html += `<button onclick="showInfoPokemons('${pokemon.pokemon.name}')">${pokemon.pokemon.name}</button>`
-        
-        sectionPokemons.innerHTML = html
+
+        sectionPokemons.innerHTML = buttonToBack
+        sectionPokemons.innerHTML += html
         sectionInfoPokemons.innerHTML = ""
 
     });
-    
+
 }
 
-const showInfoPokemons = async(pokemon) => {
+const showInfoPokemons = async (pokemon) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     const json = await response.json()
+    console.log(json)
 
-    let html =""
-    html += `<button onclick="showInfoPokemons('')"><-</button>`
-        
-        
-    const srcImg = json["sprites"]["front_default"]
-    
-    let button = ` <button onclick="toBack()" class="toBack"><-</button>`
+    const buttonToBack = `<button onclick="toBack()" class="toBack"><-</button>`
+
+    const srcImg = json["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"]
 
     let divStats = `<hr><h3 id="h3Stats">Stats</h3>`
     const stats = json.stats
@@ -74,18 +77,18 @@ const showInfoPokemons = async(pokemon) => {
         
         <div class="characteristics">
             <p class="statsName">${statsNames[item.stat.name]}</p>
-
+            
             <p class="statsValue">${item.base_stat}</p>
 
             <div class="line-bar">
-                <div class="bar" style="width: ${item.base_stat}px"></div>
+            <div class="bar" style="width: ${item.base_stat}px"></div>
             </div>
-        </div>
-        `
+            </div>
+            `
     });
-    
+
     const type = json.types
-    
+
     let divType = ""
 
     type.forEach(item => {
@@ -97,37 +100,51 @@ const showInfoPokemons = async(pokemon) => {
         <h1>${json.name}</h1>   
         <img src="${srcImg}" alt="pokemon">
         <div id="divType">${divType}</div>
-    </section>
-    `
-    
-    let infoPokemons =`
+        </section>
+        `
 
-    
-    <section id="info">
-    
-    <h3>About</h3>
+    let infoPokemons = `
+        
+        
+        <section id="info">
+        
+        <h3>About</h3>
         <section id="about">
-            <div>
-                <div>${parseFloat(json.weight).toFixed(2).replace(".",",")} kg</div>
-                <p>weight</p>
-            </div>
-            <div>
-                <div>${parseFloat(json.height).toFixed(2).replace(".",",")} cm</div>
-                <p>height</p>
-            </div>
+        <div>
+        <div>${transformMeasures(json.weight)} kg</div>
+        <p>weight</p>
+        </div>
+        <div>
+        <div>${transformMeasures(json.height)} cm</div>
+        <p>height</p>
+        </div>
         </section>
-
+        
         <section id="stats">
-            <div id="data">
-                ${divStats}
-            </div>
+        <div id="data">
+        ${divStats}
+        </div>
         </section>
-    </section>
-    
-    `
+        </section>
+        `
+
+    const render = `${buttonToBack}
+        <div id="pokemonData">
+        ${pokemonPerfil}
+        ${infoPokemons}
+        </div>`
 
     sectionPokemons.innerHTML = ""
-    sectionInfoPokemons.innerHTML = pokemonPerfil
-    sectionInfoPokemons.innerHTML += infoPokemons
+    sectionInfoPokemons.innerHTML = render
 
 }
+
+const toBack = () => {
+    showPokemons(lastPokemonTypePicked)
+}
+
+const transformMeasures = (value) => {
+    return parseFloat(value).toFixed(2).replace(".", ",") || '0,00'
+}
+
+init()
